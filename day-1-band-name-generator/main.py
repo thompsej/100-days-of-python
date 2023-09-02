e43e2ac62fa326f86f8f97335a1ea8a062592c5f
Edmond Thompson <edmond.j.thompson@gmail.com>
2023-09-01 20:30:59 -0400

    band-name-generator

---
+++ day-1-band-name-generator/main.py
@@ -0,0 +1,6 @@
+print("Welcome to the Band Name Generator!")
+city = input("What city did you grow up in?\n")
+pet = input ("What is the name of your pet?\n")
+band_name = city + " " + pet
+band_name_output = f"Your band name is {band_name}!"
+print(band_name_output)
